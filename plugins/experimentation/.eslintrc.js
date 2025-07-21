1ae538a781031b51428e7e96574ede00a9f11c6c
Xinyi Feng <xfeng@adobe.com>
2025-07-21 09:58:45 -0700

    fix lint

--- plugins/experimentation/.eslintrc.js
+++ plugins/experimentation/.eslintrc.js
@@ -1,21 +1,37 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
+  extends: ["airbnb-base", "plugin:json/recommended"],
   env: {
     browser: true,
   },
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     allowImportExportEverywhere: true,
-    sourceType: 'module',
+    sourceType: "module",
     requireConfigFile: false,
   },
   rules: {
     // allow reassigning param
-    'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
-    'import/extensions': ['error', {
-      js: 'always',
-    }],
+    "no-param-reassign": [2, { props: false }],
+    "linebreak-style": ["error", "unix"],
+    "import/extensions": [
+      "error",
+      {
+        js: "always",
+      },
+    ],
+    // allow importing from these modules
+    "import/no-unresolved": [
+      "error",
+      {
+        ignore: [
+          "@playwright/test",
+          "monocart-coverage-reports",
+          "./lib-franklin.js",
+          "./cookie-consent/lib-cookie-consent.js",
+          "./analytics/lib-analytics.js",
+        ],
+      },
+    ],
   },
 };
